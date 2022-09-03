848811073c09039e27ac6dd6df09ce37053b4290
manisharpatil <manisharpatil6@gmail.com>
2022-09-04 02:08:28 +0530

    feat: react-responsive-website

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/logo.svg'; 
+import './navbar.css';
+
+//BEM -> Block Element Modifier - css class format I am using it below
+
+const Navbar = () => {
+    return (
+        <div className='gpt3__navbar'>
+            <div className='gpt3__navbar-links'>
+                <div className='gpt3__navbar-links_logo'>
+                    <img src={logo} alt="logo" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
